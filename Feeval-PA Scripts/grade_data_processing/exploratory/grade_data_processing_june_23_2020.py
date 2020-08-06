772bde38f909467b896545fd31aa5ea7b1ffa033
Apoorb <apoorb2510@gmail.com>
2020-08-06 15:04:07 -0400

    Reorganize

--- Feeval-PA Scripts/grade_data_processing/exploratory/grade_data_processing_june_23_2020.py
+++ Feeval-PA Scripts/grade_data_processing/exploratory/grade_data_processing_june_23_2020.py
@@ -1,103 +1,248 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  4 16:42:33 2020
 
+@author: abibeka
+"""
 
-# #0.0 Housekeeping. Clear variable space
-# from IPython import get_ipython  #run magic commands
-# ipython = get_ipython()
-# ipython.magic("reset -f")
-# ipython = get_ipython()
+# 0 Housekeeping. Clear variable space
+# run magic commands
+from IPython import get_ipython
 
-import pandas as pd, geopandas as gpd, fiona, os, inflection, matplotlib.pyplot as plt, pyepsg, folium, math
+ipython = get_ipython()
+ipython.magic("reset -f")
+ipython = get_ipython()
+
+# 1 Import Libraries and Set Global Parameters
+# 1.1 Import Libraries
+import pandas as pd
+import geopandas as gpd
+import fiona
+import os
+import matplotlib.pyplot as plt
+import pyepsg
+import folium
+import numpy as np
+from shapely.geometry import MultiLineString, mapping, shape
 from folium import plugins
+import math
 
-path_to_data = r'C:\Users\abibeka\OneDrive - Kittelson & Associates, Inc\Documents\Passive Projects' \
-               r'\Freeval-PA\grade_data\June_23_2020'
+from grade_data_process_functions import read_data
+from grade_data_process_functions import read_data
+
+# 1.2 Set Global Parameters
+path_to_data = (r'C:\Users\abibeka\OneDrive - Kittelson & Associates, Inc'
+                r'\Documents\Passive Projects\Freeval-PA\grade_data'
+                r'\June_23_2020')
 path_to_grade_data_file = os.path.join(path_to_data, 'Processing.gdb')
 fiona.listlayers(path_to_grade_data_file)
 
-# gpdfreeval_dat = gpd.read_file(filename=path_to_grade_data_file, layer='FREEVAL_80_Butler')
-# freeval_grade_penndot = gpd.read_file(filename=path_to_grade_data_file, layer='FREEVAL_GRADE_PENNDOT')
-# spat_join_eb_dat = gpd.read_file(filename=path_to_grade_data_file, layer='SpatialJoin80EB')
-spat_join_eb_tot_dat = gpd.read_file(filename=path_to_grade_data_file, layer='SpatialJoinTotal80EB')
-# spat_join_eb_tot_dat_temp = pd.DataFrame(spat_join_eb_tot_dat.drop(columns='geometry'))
-temp = gpd.read_file(filename=path_to_grade_data_file, layer='SpaitialJoinTotal80EBTable')
+# 2 read data
+# -----------------------------------------------------------------------------
 
-# read data
-def read_data(filename_gdf, layer_gdf, sort_fkey_asc=True):
-    gdf = gpd.read_file(filename=filename_gdf, layer=layer_gdf)
-    gdf.columns = [inflection.underscore(colname) for colname in gdf.columns]
-    subset_gdf = gdf[['name','juris','cty_code','st_rt_no','seg_bgn','seg_end','seg_no',
-                      'side_ind','seg_lngth_','seg_lngth_2','seq_no','fkey', 'flength','foffset','seg_bgn_1','offset_bgn_1',
-                      'seg_end_1','offset_end_1','side_ind_1','terrain_ty','fgrade','c_offset_b','cum_offset','c_offset_1',
-                      'cum_offs_1','nlf_cntl_b','nlf_cntl_e','geometry']]
-    subset_gdf.sort_values(by=['name', 'fkey'], inplace=True,ascending = ['True',sort_fkey_asc])
-    return subset_gdf
 
-i80_eb_sub_gdf = read_data(filename_gdf=path_to_grade_data_file,
-                           layer_gdf='SpatialJoinTotal80EB')
-i80_eb_sub_gdf.crs
+grade_gdf = read_data(
+    filename_gdf=path_to_grade_data_file,
+    layer_gdf='SpatialJoin_GradeDataFINAL'
+)
 
-i80_eb_sub_gdf.loc[:,'foffset_diff'] = i80_eb_sub_gdf.groupby(['name','seg_bgn_1']).foffset.diff()
-i80_eb_sub_gdf.loc[:,'foffset_diff_max'] = \
-    i80_eb_sub_gdf.groupby(['name']).foffset_diff.transform(lambda x: x.abs().max())
+grade_gdf_asc_sort = (
+    grade_gdf
+        .loc[lambda x: x.seg_no.astype(int) % 2 == 0]
+        .sort_values(by=['name', 'fkey'], ascending=[True, True])
+        .reset_index(drop=True)
+)
 
-i80_eb_sub_gdf_debug = i80_eb_sub_gdf.query('foffset_diff_max>100')
+grade_gdf_desc_sort = (
+    grade_gdf
+        .loc[lambda x: x.seg_no.astype(int) % 2 != 0]
+        .sort_values(by=['name', 'fkey'], ascending=[True, False])
+        .reset_index(drop=True)
+)
 
-gjson = i80_eb_sub_gdf_debug.to_json()
-m = folium.Map([-15.783333, -47.866667],
-                  zoom_start=4,
-                  tiles='cartodbpositron')
+# Check Directions
+print("Direction for Ascending data\n",
+      grade_gdf_asc_sort.dir_ind.value_counts())
 
-from shapely.geometry import  MultiLineString, mapping, shape
+grade_gdf_desc_sort.dir_ind.value_counts()
+print("Direction for Descending data\n",
+      grade_gdf_desc_sort.dir_ind.value_counts())
 
-m = folium.Map(tiles='cartodbpositron')
-popup_field_list = list(i80_eb_sub_gdf_debug.columns)
+grade_df_asc = pd.DataFrame(grade_gdf_asc_sort.drop(columns='geometry'))
+grade_df_desc = pd.DataFrame(grade_gdf_desc_sort.drop(columns='geometry'))
+
+grade_df_asc.to_csv(os.path.join(path_to_data, "grade_asc_data.csv"))
+grade_df_desc.to_csv(os.path.join(path_to_data, "grade_desc_data.csv"))
+
+grade_gdf_asc_sort.loc[:, 'foffset_diff'] = (
+    grade_gdf_asc_sort.groupby(['name', 'seg_no'])
+        .c_offset_b.diff()
+)
+
+grade_gdf_asc_sort.loc[:, 'foffset_diff_max'] = (
+    grade_gdf_asc_sort
+        .groupby(['name']).foffset_diff
+        .transform(lambda x: x.abs().max())
+)
+
+grade_gdf_asc_sort.loc[:, 'c_offset_b_diff'] = (
+    grade_gdf_asc_sort.groupby(['name'])
+        .c_offset_b.diff()
+)
+
+grade_gdf_asc_sort.loc[:, 'c_offset_b_diff_max'] = (
+    grade_gdf_asc_sort
+        .groupby(['name']).foffset_diff
+        .transform(lambda x: x.abs().max())
+)
+
+grade_gdf_asc_sort.loc[:, 'count_rows'] = (
+    grade_gdf_asc_sort
+        .groupby(['name']).c_offset_b_diff
+        .transform(lambda x: x.count())
+)
+
+temp = grade_gdf_asc_sort.head(10000)
+
+check_freeval_seg = grade_gdf_asc_sort.query('c_offset_b_diff>1000').name.unique()
+
+grade_df_debug = grade_gdf_asc_sort.query("name in @check_freeval_seg")
+
+check_freeval_seg_30 = check_freeval_seg[0:10]
+grade_df_debug_1 = grade_gdf_asc_sort.query("name in @check_freeval_seg_30")
+grade_df_debug_1 = grade_df_debug_1.query("c_offset_b_diff != 0")
+
+m = folium.Map(tiles='cartodbdark_matter', zoom_start=16, max_zoom=25, control_scale=True)
+
+esri_imagery = "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+esri_attribution = \
+    "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, " \
+    "UPR-EGP, and the GIS User Community"
+folium.TileLayer(name="EsriImagery", tiles=esri_imagery, attr=esri_attribution,
+                 zoom_start=16, max_zoom=25, control_scale=True).add_to(m)
+folium.TileLayer('cartodbpositron', zoom_start=16, max_zoom=20, control_scale=True).add_to(m)
+folium.TileLayer('openstreetmap', zoom_start=16, max_zoom=20, control_scale=True).add_to(m)
+
+popup_field_list = list(grade_df_debug_1.columns)
 popup_field_list.remove('geometry')
-feature_grp = folium.FeatureGroup(name='freeval_name')
-m.add_child(master_feature_grp)
-for i, row in i80_eb_sub_gdf_debug.iterrows():
-    sub_feature_grp = \
-        plugins.FeatureGroupSubGroup(feature_grp,
-                                     f"{row["name"]}")
-    m.add_child(sub_feature_grp)
-    temp_grp = "test"
-    label = '<br>'.join([field + ': ' + str(row[field]) for field in popup_field_list])
+
+name_feature_grp = {}
+for freeval_name in grade_df_debug_1.name.unique():
+    name_feature_grp[freeval_name] = \
+        folium.FeatureGroup(name=freeval_name)
+    m.add_child(name_feature_grp[freeval_name])
+
+for i, row in grade_df_debug_1.iterrows():
+    label = '<br>'.join([field + ': ' + str(row[field])
+                         for field in popup_field_list])
     # https://deparkes.co.uk/2019/02/27/folium-lines-and-markers/
-    line_points = [(tuples[1],tuples[0]) for tuples in list(mapping(row.geometry)['coordinates'][0])]
-    if math.isclose(row.foffset_diff, row.foffset_diff_max,rel_tol=1e-2):
-        color_line = 'red'; opacity_line = 1; weight_line = 6
+    line_points = [
+        (tuples[1], tuples[0])
+        for tuples in list(mapping(row.geometry)['coordinates'][0])
+    ]
+    if math.isclose(row.c_offset_b_diff, row.c_offset_b_diff_max, rel_tol=1e-2):
+        color_line = 'red';
+        opacity_line = 1;
+        weight_line = 6
     else:
-        color_line='blue'; opacity_line=0.5; weight_line = 3
-    folium.PolyLine(line_points, color=color_line, weight=weight_line, opacity=opacity_line \
-                    , popup=folium.Popup(html=label, parse_html=False, max_width='300')).add_to(sub_feature_grp)
-folium.LayerControl(collapsed=True).add_to(m)
-m.save(os.path.join(path_to_data,'test.html'))
-#https://gis.stackexchange.com/questions/104312/multilinestring-to-separate-individual-lines-using-python-with-gdal-ogr-fiona
+        color_line = 'blue';
+        opacity_line = 0.5;
+        weight_line = 3
+
+    folium.PolyLine(
+        line_points,
+        color=color_line,
+        weight=weight_line,
+        opacity=opacity_line,
+        popup=folium.Popup(
+            html=label,
+            parse_html=False,
+            max_width='300'
+        )
+    ).add_to(name_feature_grp[f"{row['name']}"])
+folium.LayerControl(collapsed=False).add_to(m)
+
+m.save(os.path.join(path_to_data, 'check_big_offset_jumps.html'))
+
+grade_gdf_asc_sort_clean = grade_gdf_asc_sort.query("c_offset_b_diff < 100")
+check_freeval_seg = grade_gdf_asc_sort_clean.name.unique()
+
+grade_df_debug = grade_gdf_asc_sort_clean.query("name in @check_freeval_seg")
+
+check_freeval_seg_100 = check_freeval_seg[0:200]
+grade_df_debug_1 = grade_gdf_asc_sort_clean.query("name in @check_freeval_seg_100")
+grade_df_debug_1 = grade_df_debug_1.query("c_offset_b_diff != 0")
+
+m = folium.Map(tiles='cartodbdark_matter', zoom_start=16, max_zoom=25, control_scale=True)
+
+esri_imagery = "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+esri_attribution = \
+    "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, " \
+    "UPR-EGP, and the GIS User Community"
+folium.TileLayer(name="EsriImagery", tiles=esri_imagery, attr=esri_attribution,
+                 zoom_start=16, max_zoom=25, control_scale=True).add_to(m)
+folium.TileLayer('cartodbpositron', zoom_start=16, max_zoom=20, control_scale=True).add_to(m)
+folium.TileLayer('openstreetmap', zoom_start=16, max_zoom=20, control_scale=True).add_to(m)
+
+popup_field_list = list(grade_df_debug_1.columns)
+popup_field_list.remove('geometry')
+
+name_feature_grp = {}
+for freeval_name in grade_df_debug_1.name.unique():
+    name_feature_grp[freeval_name] = \
+        folium.FeatureGroup(name=freeval_name)
+    m.add_child(name_feature_grp[freeval_name])
+
+for i, row in grade_df_debug_1.iterrows():
+    label = '<br>'.join([field + ': ' + str(row[field])
+                         for field in popup_field_list])
+    # https://deparkes.co.uk/2019/02/27/folium-lines-and-markers/
+    line_points = [
+        (tuples[1], tuples[0])
+        for tuples in list(mapping(row.geometry)['coordinates'][0])
+    ]
+    if row.c_offset_b_diff > 500:
+        color_line = 'red';
+        opacity_line = 1;
+        weight_line = 6
+    else:
+        color_line = 'blue';
+        opacity_line = 0.5;
+        weight_line = 3
+
+    folium.PolyLine(
+        line_points,
+        color=color_line,
+        weight=weight_line,
+        opacity=opacity_line,
+        popup=folium.Popup(
+            html=label,
+            parse_html=False,
+            max_width='300'
+        )
+    ).add_to(name_feature_grp[f"{row['name']}"])
+folium.LayerControl(collapsed=False).add_to(m)
+
+m.save(os.path.join(path_to_data, 'check_all_offsets.html'))
+
+# https://gis.stackexchange.com/questions/104312/multilinestring-to-separate-individual-lines-using-python-with-gdal-ogr-fiona
 # for line in row.geometry:
 #     [print(tuples) for tuples in list(line)]
 
 
-i80_eb_sub_gdf.groupby(['name']).cty_code.apply(lambda x: len(set(x))).max()
-
-i80_wb_sub_gdf = read_data(filename_gdf=path_to_grade_data_file,
-                           layer_gdf='SpatialJoinTotal80WB',
-                           sort_fkey_asc=False)
-
-with pd.option_context("display.max_rows", 10, "display.max_columns", 20):
-    print(i80_eb_sub_gdf)
-
-# clean data
-
-
 # reshape data
 
-# inventory of freeval segments wtih terrain type 1 and 2---length weighted average
+# inventory of freeval segments wtih terrain type 1 and 2---length weighted
+# average
 
 
 # inventory of freeval segments crossing county lines
 
-# add extra tags to freeval segments when they cross county lines---will be broken manually
+# add extra tags to freeval segments when they cross county lines
+# ---will be broken manually
 
-# inventory of freeval segments that need to be broken into two (mountaineous terrain)
+# inventory of freeval segments that need to be broken into two
+# (mountaineous terrain)
 
 ## add extra tags to freeval segments when grade changes over 2%
 

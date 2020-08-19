508e648de71cd0996a4bbb35da9756982da666e3
Apoorb <apoorb2510@gmail.com>
2020-08-18 22:16:32 -0400

    Move things to class

--- Feeval-PA Scripts/grade_data_processing/01_grade_data_read_test.py
+++ Feeval-PA Scripts/grade_data_processing/01_grade_data_read_test.py
@@ -17,112 +17,97 @@ path_to_data = (r'C:\Users\abibeka\OneDrive - Kittelson & Associates, Inc'
                 r'\June_23_2020')
 path_to_grade_data_file = os.path.join(path_to_data, 'Processing.gdb')
 path_processed_data = os.path.join(path_to_data, "processed_data")
-if not os.path.exists(path_processed_data):
-    os.mkdir(path_processed_data)
-# 2 read data and output smaller subsets
-# -----------------------------------------------------------------------------
-grade_df_dict = gradepr.data_read_switch(
-    path_to_data=path_to_data,
-    path_to_grade_data_file=path_to_grade_data_file,
-    path_processed_data=path_processed_data,
-    read_saved_shp_csv=True,
-    read_saved_csv=False,
-)
-
-grade_df_asc = grade_df_dict['grade_df_asc']
-grade_df_asc_sort = (grade_df_asc
-                     .sort_values(['name', 'cty_code', 'fkey'],
-                                  ascending=[True, True, True])
-                     )
-grade_df_asc_sort_unique = grade_df_asc_sort.drop_duplicates(
-    subset=["name", "cty_code", "seg_no", "foffset"],
-    keep="first",
-    ignore_index=True)
-# Quick test
-grade_df_asc_sort_unique.groupby(["st_rt_no", "cty_code"]).name.apply(set)
-
-# 3. Run tests on I-83 data
-# -----------------------------------------------------------------------------
-# Subset and sort data for route 83 by fseg and foffset
-grade_df_asc_sort_83 = (grade_df_asc_sort
-                        .query("st_rt_no == 83")
-                        .sort_values(["fseg", "foffset"])
-                        )
-# Missing fseg where causing issue when we groupby fseg and sum foffset.
-grade_df_asc_sort_83_unique = (
-    grade_df_asc_sort_83
-    .loc[~ grade_df_asc_sort_83.fseg.isna()]
-    .drop_duplicates(["fseg", "foffset"])
-    )
-# Test if the total length of I-83 is same (close) using foffset, c_offset_b,
-# and flength
-grade_df_asc_sort_83_unique = (
-    grade_df_asc_sort_83_unique
-    .assign(c_offset_b_diff=lambda x: x
-            .groupby(["cty_code", "seg_no"]).c_offset_b.diff(),
-            foffset_diff=lambda x: x
-            .groupby(["fseg"]).foffset.diff(),
-            name_diff=lambda x: x.name.diff())
-    )
-grade_df_asc_sort_83_unique[
-        ["flength", "foffset_diff", "c_offset_b_diff"]
-    ].sum()
-grade_df_asc_sort_83_unique[
-        ["name_diff", "foffset_diff", "c_offset_b_diff"]
-    ].describe()
-# Test if the name increases along with fseg and foffset
-check_name_order = grade_df_asc_sort_83_unique.loc[lambda x: x.name_diff < 0]
-# Test if fkeg increases within a county
-grade_df_asc_sort_83_unique.groupby(["cty_code"]).fkey.diff().describe()
-# Test if seq_no increases irrespective of county along route I-83
-grade_df_asc_sort_83_unique.seq_no.diff().describe()
-
-# Subset and sort data for route 83 by name, city_code, and fkey; Alexandra's
-# method
-grade_df_asc_sort_83_fkey = (
-    grade_df_asc_sort
-    .query("st_rt_no == 1")
-    .loc[~ grade_df_asc_sort.fkey.isna()]
-    .sort_values(["name", "cty_code", "fkey"])
-    .drop_duplicates(["fseg", "foffset"])
-    )
-# Test if the total length of I-83 is same (close) using foffset, c_offset_b,
-# and flength
-grade_df_asc_sort_83_fkey = (
-    grade_df_asc_sort_83_fkey
-    .assign(c_offset_b_diff=lambda x: x
-            .groupby(["cty_code", "seg_no"]).c_offset_b.diff(),
-            foffset_diff=lambda x: x
-            .groupby(["cty_code", "fseg"]).foffset.diff(),
-            name_diff=lambda x: x
-            .groupby(["cty_code"]).name.diff())
-    )
-grade_df_asc_sort_83_fkey[
-        ["flength", "foffset_diff", "c_offset_b_diff"]
-    ].sum()
-# Test if the name, foffset, and c_offset_b increases along with fseg
-# and foffset
-grade_df_asc_sort_83_fkey[
-        ["name_diff", "foffset_diff", "c_offset_b_diff"]
-    ].describe()
-
-# Need to drop duplicates to make the flength sum similar to
-# foffset_diff, and c_offset_diff
-
-# inventory of freeval segments wtih terrain type 1 and 2---length weighted
-# average
 
 
-# inventory of freeval segments crossing county lines
+if __name__ == "__main__":
+    if not os.path.exists(path_processed_data):
+        os.mkdir(path_processed_data)
+    read_obj = gradepr.ReadGrade(
+        path_to_data=path_to_data,
+        path_to_grade_data_file=path_to_grade_data_file,
+        path_processed_data=path_processed_data,
+        read_saved_shp_csv=True,
+        read_saved_csv=False)
+    # 2 read data and output smaller subsets
+    # -----------------------------------------------------------------------------
+    grade_df_dict = read_obj.data_read_switch()
 
-# add extra tags to freeval segments when they cross county lines
-# ---will be broken manually
+    grade_df_asc = grade_df_dict['grade_df_asc']
+    grade_df_asc_sort = (grade_df_asc
+                         .sort_values(['name', 'cty_code', 'fkey'],
+                                      ascending=[True, True, True])
+                         )
+    grade_df_asc_sort_unique = grade_df_asc_sort.drop_duplicates(
+        subset=["name", "cty_code", "seg_no", "foffset"],
+        keep="first",
+        ignore_index=True)
+    # Quick test
+    grade_df_asc_sort_unique.groupby(["st_rt_no", "cty_code"]).name.apply(set)
 
-# inventory of freeval segments that need to be broken into two
-# (mountaineous terrain)
+    # 3. Run tests on I-83 data
+    # -----------------------------------------------------------------------------
+    # Subset and sort data for route 83 by fseg and foffset
+    grade_df_asc_sort_83 = (grade_df_asc_sort
+                            .query("st_rt_no == 83")
+                            .sort_values(["fseg", "foffset"])
+                            )
+    # Missing fseg where causing issue when we groupby fseg and sum foffset.
+    grade_df_asc_sort_83_unique = (
+        grade_df_asc_sort_83
+        .loc[~ grade_df_asc_sort_83.fseg.isna()]
+        .drop_duplicates(["fseg", "foffset"])
+        )
+    # Test if the total length of I-83 is same (close) using foffset,
+    # c_offset_b,
+    # and flength
+    grade_df_asc_sort_83_unique = (
+        grade_df_asc_sort_83_unique
+        .assign(c_offset_b_diff=lambda x: x
+                .groupby(["cty_code", "seg_no"]).c_offset_b.diff(),
+                foffset_diff=lambda x: x
+                .groupby(["fseg"]).foffset.diff(),
+                name_diff=lambda x: x.name.diff())
+        )
+    grade_df_asc_sort_83_unique[
+            ["flength", "foffset_diff", "c_offset_b_diff"]
+        ].sum()
+    grade_df_asc_sort_83_unique[
+            ["name_diff", "foffset_diff", "c_offset_b_diff"]
+        ].describe()
+    # Test if the name increases along with fseg and foffset
+    check_name_order = grade_df_asc_sort_83_unique.loc[lambda x: x.name_diff < 0]
+    # Test if fkeg increases within a county
+    grade_df_asc_sort_83_unique.groupby(["cty_code"]).fkey.diff().describe()
+    # Test if seq_no increases irrespective of county along route I-83
+    grade_df_asc_sort_83_unique.seq_no.diff().describe()
 
-# add extra tags to freeval segments when grade changes over 2%
-
-# process freeval segments with terrain type 1 and 2
-
-# process freeval segments with terrain type 3
+    # Subset and sort data for route 83 by name, city_code, and fkey;
+    # Alexandra's
+    # method
+    grade_df_asc_sort_83_fkey = (
+        grade_df_asc_sort
+        .query("st_rt_no == 1")
+        .loc[~ grade_df_asc_sort.fkey.isna()]
+        .sort_values(["name", "cty_code", "fkey"])
+        .drop_duplicates(["fseg", "foffset"])
+        )
+    # Test if the total length of I-83 is same (close) using foffset,
+    # c_offset_b,
+    # and flength
+    grade_df_asc_sort_83_fkey = (
+        grade_df_asc_sort_83_fkey
+        .assign(c_offset_b_diff=lambda x: x
+                .groupby(["cty_code", "seg_no"]).c_offset_b.diff(),
+                foffset_diff=lambda x: x
+                .groupby(["cty_code", "fseg"]).foffset.diff(),
+                name_diff=lambda x: x
+                .groupby(["cty_code"]).name.diff())
+        )
+    grade_df_asc_sort_83_fkey[
+            ["flength", "foffset_diff", "c_offset_b_diff"]
+        ].sum()
+    # Test if the name, foffset, and c_offset_b increases along with fseg
+    # and foffset
+    grade_df_asc_sort_83_fkey[
+            ["name_diff", "foffset_diff", "c_offset_b_diff"]
+        ].describe()
--- Feeval-PA Scripts/grade_data_processing/grade_process_mod/read_grade.py
+++ Feeval-PA Scripts/grade_data_processing/grade_process_mod/read_grade.py
@@ -6,222 +6,227 @@ import glob
 import fiona
 import shutil
 
-def data_read_switch(path_to_data, path_to_grade_data_file,
-                     path_processed_data,
-                     read_saved_shp_csv=False,
-                     read_saved_csv=True):
-    """
+class ReadGrade:
 
-    Parameters
-    ----------
-    path_to_data
-    path_to_grade_data_file
-    path_processed_data
-    read_saved_shp_csv
-    read_saved_csv
+    def __init__(self, path_to_data, path_to_grade_data_file,
+                         path_processed_data,
+                         read_saved_shp_csv=False,
+                         read_saved_csv=True):
+        self.path_to_data = path_to_data
+        self.path_processed_data = path_processed_data
+        self.path_to_grade_data_file = path_to_grade_data_file
+        self.layer_raw_grade_data = 'SpatialJoin_GradeDataFINAL'
+        self.read_saved_shp_csv = read_saved_shp_csv
+        self.read_saved_csv = read_saved_csv
 
-    Returns
-    -------
+    def data_read_switch(self):
+        """
 
-    """
-    if (read_saved_shp_csv 
-        & (len(glob.glob(
-            os.path.join(path_processed_data,
-                             "grade_gdf_asc_sort", "*.shp"))) == 1)
-        & len(glob.glob(
-            os.path.join(path_processed_data,
-                             "grade_gdf_desc_sort", "*.shp"))) == 1):
-        # Read the data if above conditions met
-        grade_gdf_asc_sort = read_subset_dat_by_dir(
-            path_processed_data=path_processed_data,
-            shapefile_nm="grade_gdf_asc_sort"
-        )
-        grade_gdf_desc_sort = read_subset_dat_by_dir(
-            path_processed_data=path_processed_data,
-            shapefile_nm="grade_gdf_desc_sort"
-        )
-        grade_df_asc = pd.read_csv(os.path.join(
-            path_to_data,
-            "processed_data", "grade_asc_data.csv"), index_col=0)
-        grade_df_desc = pd.read_csv(os.path.join(
-            path_to_data,
-            "processed_data", "grade_desc_data.csv"), index_col=0)
-        return_dict ={
-            "grade_gdf_asc_sort":grade_gdf_asc_sort,
-            "grade_gdf_desc_sort":grade_gdf_desc_sort,
-            "grade_df_asc":grade_df_asc,
-            "grade_df_desc":grade_df_desc
-        }
-        return return_dict
-    elif not read_saved_csv:
-        if (len(glob.glob(
-            os.path.join(path_processed_data,
-                             "grade_gdf_asc_sort", "*.shp"))) == 1)\
+        Parameters
+        ----------
+        path_to_data
+        path_to_grade_data_file
+        path_processed_data
+        read_saved_shp_csv
+        read_saved_csv
+
+        Returns
+        -------
+
+        """
+        if (self.read_saved_shp_csv
             & (len(glob.glob(
-            os.path.join(path_processed_data,
-                             "grade_gdf_desc_sort", "*.shp"))) == 1):
-                Y_N = input("Directly reading GIS geodatabase is expensive."
-                      "You have smaller subset of data saved as .shp."
-                      "and .csv./n"
-                      "Do you want to continue this read operation: (Y/N)")
-                if Y_N.upper() == "N":
-                    print("Use the saved .shp or .csv file parameters!")
-                    return
-                elif Y_N.upper() != "N":
-                    raise ValueError("Enter Y or N next time!")
-        fiona.listlayers(path_to_grade_data_file)
-        grade_gdf = read_raw_data(
-            filename_gdf=path_to_grade_data_file,
-            layer_gdf='SpatialJoin_GradeDataFINAL'
-        )
-        grade_gdf_asc_sort, grade_gdf_desc_sort = create_subset_dat(
-            grade_gdf=grade_gdf
-        )
-        save_subset_dat_by_dir(
-            grade_gdf_asc_sort=grade_gdf_asc_sort,
-            grade_gdf_desc_sort=grade_gdf_desc_sort,
-            path_processed_data=path_processed_data
-        )
-        grade_df_asc = pd.read_csv(os.path.join(path_processed_data,
-                                                "grade_asc_data.csv"),
-                                   index_col=0)
-        grade_df_desc = pd.read_csv(os.path.join(path_processed_data,
-                                                 "grade_desc_data.csv"),
-                                    index_col=0)
-        return_dict ={
-            "grade_gdf_asc_sort":grade_gdf_asc_sort,
-            "grade_gdf_desc_sort":grade_gdf_desc_sort,
-            "grade_df_asc":grade_df_asc,
-            "grade_df_desc":grade_df_desc
-        }
-        return return_dict
-    elif read_saved_csv:
-        grade_df_asc = pd.read_csv(os.path.join(path_processed_data,
-                                                "grade_asc_data.csv"),
-                                   index_col=0)
-        grade_df_desc = pd.read_csv(os.path.join(path_processed_data,
-                                                 "grade_desc_data.csv"),
-                                    index_col=0)
-        return_dict ={
-            "grade_df_asc":grade_df_asc,
-            "grade_df_desc":grade_df_desc
-        }
-        return return_dict
-    else:
-        print("")
+                os.path.join(self.path_processed_data,
+                                 "grade_gdf_asc_sort", "*.shp"))) == 1)
+            & len(glob.glob(
+                os.path.join(self.path_processed_data,
+                                 "grade_gdf_desc_sort", "*.shp"))) == 1):
+            # Read the data if above conditions met
+            grade_gdf_asc_sort = self.read_subset_dat_by_dir(
+                shapefile_nm="grade_gdf_asc_sort"
+            )
+            grade_gdf_desc_sort = self.read_subset_dat_by_dir(
+                shapefile_nm="grade_gdf_desc_sort"
+            )
+            grade_df_asc = pd.read_csv(os.path.join(
+                self.path_to_data,
+                "processed_data", "grade_asc_data.csv"), index_col=0)
+            grade_df_desc = pd.read_csv(os.path.join(
+                self.path_to_data,
+                "processed_data", "grade_desc_data.csv"), index_col=0)
+            return_dict ={
+                "grade_gdf_asc_sort":grade_gdf_asc_sort,
+                "grade_gdf_desc_sort":grade_gdf_desc_sort,
+                "grade_df_asc":grade_df_asc,
+                "grade_df_desc":grade_df_desc
+            }
+            return return_dict
+        elif not self.read_saved_csv:
+            if (len(glob.glob(os.path.join(
+                    self.path_processed_data,
+                    "grade_gdf_asc_sort", "*.shp"))) == 1)\
+                    & (len(glob.glob(os.path.join(
+                    self.path_processed_data,
+                    "grade_gdf_desc_sort", "*.shp"))) == 1):
+                    # if block begins
+                    y_n = input("Directly reading GIS geodatabase is "
+                                    "expensive."
+                                    "You have smaller subset of data saved as "
+                                    ".shp. and .csv./n"
+                                    "Do you want to continue this read "
+                                    "operation: (Y/N)")
+                    if y_n.upper() == "N":
+                        print("Use the saved .shp or .csv file parameters!")
+                        return
+                    elif y_n.upper() != "N":
+                        raise ValueError("Enter Y or N next time!")
+            #fiona.listlayers(self.path_to_grade_data_file)
+            grade_gdf = self.read_raw_data()
+            grade_gdf_asc_sort, grade_gdf_desc_sort = self.create_subset_dat(
+                grade_gdf=grade_gdf)
+            self.save_subset_dat_by_dir(
+                grade_gdf_asc_sort=grade_gdf_asc_sort,
+                grade_gdf_desc_sort=grade_gdf_desc_sort)
+            grade_df_asc = pd.read_csv(os.path.join(self.path_processed_data,
+                                                    "grade_asc_data.csv"),
+                                       index_col=0)
+            grade_df_desc = pd.read_csv(os.path.join(self.path_processed_data,
+                                                     "grade_desc_data.csv"),
+                                        index_col=0)
+            return_dict ={
+                "grade_gdf_asc_sort":grade_gdf_asc_sort,
+                "grade_gdf_desc_sort":grade_gdf_desc_sort,
+                "grade_df_asc":grade_df_asc,
+                "grade_df_desc":grade_df_desc
+            }
+            return return_dict
+        elif self.read_saved_csv:
+            grade_df_asc = pd.read_csv(os.path.join(self.path_processed_data,
+                                                    "grade_asc_data.csv"),
+                                       index_col=0)
+            grade_df_desc = pd.read_csv(os.path.join(self.path_processed_data,
+                                                     "grade_desc_data.csv"),
+                                        index_col=0)
+            return_dict ={
+                "grade_df_asc":grade_df_asc,
+                "grade_df_desc":grade_df_desc
+            }
+            return return_dict
+        else:
+            print("")
 
+    def read_raw_data(self):
+        """
+        Parameters
+        ----------
+        filename_gdf
+        layer_gdf
 
-def read_raw_data(filename_gdf, layer_gdf):
-    """
-    Parameters
-    ----------
-    filename_gdf
-    layer_gdf
+        Returns
+        -------
+        """
+        gdf = gpd.read_file(filename=self.grade_data_file,
+                            layer=self.layer_raw_grade_data)
+        gdf.columns = [
+            inflection.underscore(colname) for colname in gdf.columns]
+        subset_gdf = gdf[['name', 'juris', 'cty_code', 'st_rt_no', 'seg_bgn',
+                          'seg_end', 'seg_no', 'side_ind', 'seg_lngth_',
+                          'seg_lngth_2', 'seq_no', 'fkey', 'fseg', 'flength',
+                          'fdir', 'foffset', 'terrain_ty', 'fgrade',
+                          'c_offset_b',
+                          'cum_offset', 'c_offset_1', 'cum_offs_1',
+                          'nlf_cntl_b',
+                          'nlf_cntl_e', 'dir_ind', 'fac_type', 'geometry']]
+        return subset_gdf
 
-    Returns
-    -------
-    """
-    gdf = gpd.read_file(filename=filename_gdf, layer=layer_gdf)
-    gdf.columns = [inflection.underscore(colname) for colname in gdf.columns]
-    subset_gdf = gdf[['name', 'juris', 'cty_code', 'st_rt_no', 'seg_bgn',
-                      'seg_end', 'seg_no', 'side_ind', 'seg_lngth_',
-                      'seg_lngth_2', 'seq_no', 'fkey', 'fseg', 'flength',
-                      'fdir', 'foffset', 'terrain_ty', 'fgrade', 'c_offset_b',
-                      'cum_offset', 'c_offset_1', 'cum_offs_1', 'nlf_cntl_b',
-                      'nlf_cntl_e', 'dir_ind', 'fac_type', 'geometry']]
-    return subset_gdf
-
-
-def create_subset_dat(grade_gdf):
-    """
-    Parameters
-    ----------
-    grade_gdf
-
-    Returns
-    -------
-
-    """
-    grade_gdf_asc_sort = (
+    def create_subset_dat(self, grade_gdf):
+        """
+        Parameters
+        ----------
         grade_gdf
-            .loc[lambda x: x.seg_no.astype(int) % 2 == 0]
-            .sort_values(by=['name', 'fseg', 'foffset'],
-                         ascending=[True, True, True])
-            .reset_index(drop=True)
-    )
-    grade_gdf_desc_sort = (
-        grade_gdf
-            .loc[lambda x: x.seg_no.astype(int) % 2 != 0]
-            .sort_values(by=['name', 'fseg', 'foffset'],
-                         ascending=[True, False, False])
-            .reset_index(drop=True)
-    )
-    return grade_gdf_asc_sort, grade_gdf_desc_sort
 
+        Returns
+        -------
 
-def save_subset_dat_by_dir(grade_gdf_asc_sort,
-                           grade_gdf_desc_sort, path_processed_data):
-    """
-    Parameters
-    ----------
-    grade_gdf_asc_sort
-    grade_gdf_desc_sort
-    path_processed_data
+        """
+        grade_gdf_asc_sort = (
+            grade_gdf
+                .loc[lambda x: x.seg_no.astype(int) % 2 == 0]
+                .sort_values(by=['name', 'fseg', 'foffset'],
+                             ascending=[True, True, True])
+                .reset_index(drop=True)
+        )
+        grade_gdf_desc_sort = (
+            grade_gdf
+                .loc[lambda x: x.seg_no.astype(int) % 2 != 0]
+                .sort_values(by=['name', 'fseg', 'foffset'],
+                             ascending=[True, False, False])
+                .reset_index(drop=True)
+        )
+        return grade_gdf_asc_sort, grade_gdf_desc_sort
 
-    Returns
-    -------
+    def save_subset_dat_by_dir(self, grade_gdf_asc_sort,
+                               grade_gdf_desc_sort):
+        """
+        Parameters
+        ----------
+        grade_gdf_asc_sort
+        grade_gdf_desc_sort
+        path_processed_data
 
-    """
-    if os.path.exists(os.path.join(path_processed_data,
-                                       "grade_gdf_asc_sort")):
-        shutil.rmtree(os.path.join(path_processed_data,
+        Returns
+        -------
+
+        """
+        if os.path.exists(os.path.join(self.path_processed_data,
+                                           "grade_gdf_asc_sort")):
+            shutil.rmtree(os.path.join(self.path_processed_data,
+                                  "grade_gdf_asc_sort"))
+        os.mkdir(os.path.join(self.path_processed_data,
                               "grade_gdf_asc_sort"))
-    os.mkdir(os.path.join(path_processed_data,
-                          "grade_gdf_asc_sort"))
-    
-    if os.path.exists(os.path.join(path_processed_data,
-                                       "grade_gdf_desc_sort")):
-        shutil.rmtree(os.path.join(path_processed_data,
+
+        if os.path.exists(os.path.join(self.path_processed_data,
+                                           "grade_gdf_desc_sort")):
+            shutil.rmtree(os.path.join(self.path_processed_data,
+                                  "grade_gdf_desc_sort"))
+        os.mkdir(os.path.join(self.path_processed_data,
                               "grade_gdf_desc_sort"))
-    os.mkdir(os.path.join(path_processed_data,
-                          "grade_gdf_desc_sort"))
-    grade_gdf_asc_sort_outfile = os.path.join(path_processed_data,
-                                              "grade_gdf_asc_sort",
-                                              "grade_gdf_asc_sort.shp")
-    grade_gdf_desc_sort_outfile = os.path.join(path_processed_data,
-                                               "grade_gdf_desc_sort",
-                                               "grade_gdf_desc_sort.shp")
-    grade_gdf_asc_sort.to_file(driver='ESRI Shapefile',
-                               filename=grade_gdf_asc_sort_outfile)
-    grade_gdf_desc_sort.to_file(driver='ESRI Shapefile',
-                                filename=grade_gdf_desc_sort_outfile)
-    grade_df_asc = pd.DataFrame(grade_gdf_asc_sort.drop(columns='geometry'))
-    grade_df_desc = pd.DataFrame(grade_gdf_desc_sort.drop(columns='geometry'))
-    grade_df_asc.to_csv(
-        os.path.join(path_processed_data, "grade_asc_data.csv"))
-    grade_df_desc.to_csv(os.path.join(path_processed_data,
-                                      "grade_desc_data.csv"))
-    return 1
+        grade_gdf_asc_sort_outfile = os.path.join(self.path_processed_data,
+                                                  "grade_gdf_asc_sort",
+                                                  "grade_gdf_asc_sort.shp")
+        grade_gdf_desc_sort_outfile = os.path.join(self.path_processed_data,
+                                                   "grade_gdf_desc_sort",
+                                                   "grade_gdf_desc_sort.shp")
+        grade_gdf_asc_sort.to_file(driver='ESRI Shapefile',
+                                   filename=grade_gdf_asc_sort_outfile)
+        grade_gdf_desc_sort.to_file(driver='ESRI Shapefile',
+                                    filename=grade_gdf_desc_sort_outfile)
+        grade_df_asc = pd.DataFrame(
+            grade_gdf_asc_sort.drop(columns='geometry'))
+        grade_df_desc = pd.DataFrame(
+            grade_gdf_desc_sort.drop(columns='geometry'))
+        grade_df_asc.to_csv(
+            os.path.join(self.path_processed_data, "grade_asc_data.csv"))
+        grade_df_desc.to_csv(os.path.join(self.path_processed_data,
+                                          "grade_desc_data.csv"))
+        return 1
 
+    def read_subset_dat_by_dir(self, shapefile_nm="grade_gdf_asc_sort"):
+        """
+        Parameters
+        ----------
+        path_processed_data : TYPE
+            DESCRIPTION.
+        shapefile_nm : TYPE, optional
+            DESCRIPTION. The default is "grade_gdf_asc_sort".
 
-def read_subset_dat_by_dir(path_processed_data,
-                           shapefile_nm="grade_gdf_asc_sort"):
-    """
-    Parameters
-    ----------
-    path_processed_data : TYPE
-        DESCRIPTION.
-    shapefile_nm : TYPE, optional
-        DESCRIPTION. The default is "grade_gdf_asc_sort".
+        Returns
+        -------
+        gdf : TYPE
+            DESCRIPTION.
 
-    Returns
-    -------
-    gdf : TYPE
-        DESCRIPTION.
-
-    """
-    input_file = os.path.join(path_processed_data, shapefile_nm,
-                              f"{shapefile_nm}.shp")
-    gdf = gpd.read_file(filename=input_file)
-    return gdf
-
+        """
+        input_file = os.path.join(self.path_processed_data, shapefile_nm,
+                                  f"{shapefile_nm}.shp")
+        gdf = gpd.read_file(filename=input_file)
+        return gdf

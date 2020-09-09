936f24430272afac994c33bb4bf96d6666a8953a
Apoorba Bibeka <apoorb2510@gmail.com>
2020-09-09 12:33:31 -0400

    Use 85th percentile for terrain classification.
    Signed-off-by: Apoorba Bibeka <apoorb2510@gmail.com>

--- Feeval-PA Scripts/grade_data_processing/grade_process_mod/process_grade.py
+++ Feeval-PA Scripts/grade_data_processing/grade_process_mod/process_grade.py
@@ -85,7 +85,7 @@ class CleanGrade:
         self.test_fkey_order()
         print("**************************************************************")
 
-    def compute_grade_stats(self):
+    def compute_grade_stats(self, grade_smoothening_window=5):
         """
         Get addtional features from the correctly sorted data to help in eventually classify segment types.
         """
@@ -98,7 +98,7 @@ class CleanGrade:
             fgrade_impute=lambda df1: df1.groupby("freeval_seg_jumps")
             .fgrade.bfill()
             .ffill()
-            .rolling(12).mean(),
+            .rolling(grade_smoothening_window).mean(),
             cum_flength=lambda df1: df1.groupby("freeval_seg_jumps").flength.cumsum(),
             cum_elevation_tp=lambda df1: df1.flength * df1.fgrade_impute / 100,
             cum_elevation_relative=lambda df1: (df1.cum_elevation_tp.cumsum()),
@@ -109,6 +109,9 @@ class CleanGrade:
             .fillna(df1.fgrade_impute),
             min_freeval_seg_grade=lambda df1: df1.groupby("name")
             .fgrade_impute.transform(min)
+            .fillna(df1.fgrade_impute),
+            q85_freeval_seg_grade=lambda df1: df1.groupby("name")
+            .fgrade_impute.transform(lambda series: series.quantile(0.85))
             .fillna(df1.fgrade_impute),
             range_freeval_seg_grade=lambda df1: df1.max_freeval_seg_grade
             - df1.min_freeval_seg_grade,
@@ -140,14 +143,16 @@ class CleanGrade:
         # Get average grade by segment.
         correct_sort_df_add_stat_agg = (
             self.correct_sort_df_add_stat.filter(
-                items=["freeval_seg_jumps", "name", "avg_grade_freeval_seg"]
+                items=["freeval_seg_jumps", "name", "q85_freeval_seg_grade",
+                       "avg_grade_freeval_seg"]
             )
             .groupby(["name"])
-            .agg(avg_grade_freeval_seg=("avg_grade_freeval_seg", min))
+            .agg(q85_freeval_seg_grade=("q85_freeval_seg_grade", min),
+                 avg_grade_freeval_seg=("avg_grade_freeval_seg", min))
             .reset_index()
             .assign(
-                grade_over_2=lambda df1: df1.avg_grade_freeval_seg >= 2,
-                grade_over_3=lambda df1: df1.avg_grade_freeval_seg >= 3,
+                grade_over_2=lambda df1: df1.q85_freeval_seg_grade >= 2,
+                grade_over_3=lambda df1: df1.q85_freeval_seg_grade >= 3,
             )
         )
 
@@ -217,19 +222,21 @@ class CleanGrade:
             hcm_grade_cat=lambda df1: np.select(
                 [
                     df1.likely_vertical_curve,
+                    df1.likely_grade_change & (~ df1.grade_over_2),
                     df1.likely_grade_change,
                     (df1.seg_len_over_025 & df1.grade_over_3)
                     | (df1.seg_len_over_05 & df1.grade_over_2),
                     df1.grade_over_2,
                     (~df1.grade_over_2),
                 ],
-                ["Vertical Curve", "Grade change within segment", "Specific Grade", "Rolling", "Level"],
+                ["VMG", "level", "SMG", "Specific Grade", "Rolling", "Level"],
             )
+            #VMG: Variable mountainous grade; SMG: Specific: mountainous grade.
         ).reindex(
             columns=[
                 "name",
                 "hcm_grade_cat",
-                "avg_grade_freeval_seg",
+                "q85_freeval_seg_grade",
                 "min_freeval_seg_grade",
                 "max_freeval_seg_grade",
                 "range_freeval_seg_grade",
@@ -239,6 +246,8 @@ class CleanGrade:
                 "seg_len_over_025",
                 "seg_len",
                 "likely_vertical_curve",
+                "likely_grade_change",
+                "avg_grade_freeval_seg",
             ]
         )
         self.freeval_seg_grade_class = correct_sort_df_add_stat_agg
@@ -264,6 +273,7 @@ class CleanGrade:
                         "seg_len_over_025",
                         "seg_len",
                         "likely_vertical_curve",
+                        "likely_grade_change",
                     ]
                 ),
                 on=["name"],
@@ -295,9 +305,10 @@ class CleanGrade:
                 "fgrade_impute",
                 "cum_elevation_relative",
                 "min_freeval_seg_grade",
+                "avg_grade_freeval_seg",
+                "q85_freeval_seg_grade",
                 "max_freeval_seg_grade",
                 "range_freeval_seg_grade",
-                "avg_grade_freeval_seg",
                 "seg_len",
                 "hcm_grade_cat",
                 "grade_over_2",
@@ -306,6 +317,7 @@ class CleanGrade:
                 "seg_len_over_025",
                 "seg_len",
                 "likely_vertical_curve",
+                "likely_grade_change",
                 "cty_code",
                 "dir_ind",
                 "fkey",
@@ -327,9 +339,10 @@ class CleanGrade:
                 "fgrade_impute",
                 "cum_elevation_relative",
                 "min_freeval_seg_grade",
+                "avg_grade_freeval_seg",
+                "q85_freeval_seg_grade",
                 "max_freeval_seg_grade",
                 "range_freeval_seg_grade",
-                "avg_grade_freeval_seg",
                 "seg_len",
                 "hcm_grade_cat",
                 "grade_over_2",
@@ -338,6 +351,7 @@ class CleanGrade:
                 "seg_len_over_025",
                 "seg_len",
                 "likely_vertical_curve",
+                "likely_grade_change",
                 "cty_code",
                 "dir_ind",
                 "fkey",
@@ -608,7 +622,7 @@ if __name__ == "__main__":
     }
     df_name = "grade_df_asc"
     df = grade_df_asc
-    st_rt_no_ = 80
+    st_rt_no_ = 95
 
     asc_grade_obj = CleanGrade(
         grade_df_asc_or_desc_=df,
@@ -620,7 +634,7 @@ if __name__ == "__main__":
     )
     asc_grade_obj.clean_grade_df()
     asc_grade_obj.compute_grade_stats()
-    asc_grade_obj.plot_grade_profile(elevation_start=929)
+    asc_grade_obj.plot_grade_profile(elevation_start=0)
 
     df_name = "grade_df_desc"
     df = grade_df_desc
@@ -635,45 +649,4 @@ if __name__ == "__main__":
     )
     desc_grade_obj.clean_grade_df()
     desc_grade_obj.compute_grade_stats()
-    # desc_grade_obj.plot_grade_profile(elevation_start=336)
-
-    shuyi_dir = (
-        r"C:\Users\abibeka\OneDrive - Kittelson & Associates, Inc\Downloads" r"\I80_PA"
-    )
-    import numpy as np, os, pandas as pd
-
-    grade_uw = np.genfromtxt(
-        os.path.join(shuyi_dir, "grades_I80_PA.txt"), delimiter=","
-    )
-
-    miles_uw = np.genfromtxt(os.path.join(shuyi_dir, "miles_I80_PA.txt"), delimiter=",")
-
-    data_uw = pd.DataFrame({"milepost": miles_uw, "grade_percent": grade_uw})
-    data_uw_fil = data_uw.query("grade_percent >= -8 & grade_percent < 8")
-    data_uw_fil.loc[:, "seg_len"] = data_uw_fil.milepost.diff().fillna(0)
-    data_uw_fil.loc[:, "height"] = (data_uw_fil.seg_len * 5280) * (
-        data_uw_fil.grade_percent / 100
-    )
-    data_uw_fil.loc[:, "elevation_in_ft"] = data_uw_fil.height.cumsum() + 929
-    fig = make_subplots(
-        rows=2,
-        cols=1,
-        shared_xaxes=True,
-        subplot_titles=(
-            f"Grade Profile UW--I-80 and direction E",
-            f"Elevation UW--I-80 and direction E",
-        ),
-    )
-
-    data_grade_uw = px.line(data_uw_fil, x="milepost", y="grade_percent",)["data"]
-
-    data_elevation_uw = px.line(data_uw_fil, x="milepost", y="elevation_in_ft",)["data"]
-
-    for dat1, dat2 in zip(data_grade_uw, data_elevation_uw):
-        fig.add_trace(dat1, row=1, col=1)
-        fig.add_trace(dat2, row=2, col=1)
-    fig.update_yaxes(title_text="grade (%.)", row=1, col=1)
-    fig.update_yaxes(title_text="cum_elevation_relative (ft.)", row=2, col=1)
-    fig.update_xaxes(title_text="Milepost", row=1, col=1)
-    fig.update_xaxes(title_text="Milepost", row=2, col=1)
-    plot(fig, filename=os.path.join(path_processed_data, "uw_i_80_grade_profile.html"))
+    desc_grade_obj.plot_grade_profile(elevation_start=0)
